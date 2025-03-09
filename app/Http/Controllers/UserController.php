a4d843c5b9dda6b022a0b43f3a85ef31dfe6532f
MDTOMSDUDDIN <mdtomasuddin1@gmail.com>
2025-03-09 10:11:28 +0600

    user registration (end point complate)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,1 +1,35 @@
+<?php
 
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function UserRegistration(Request $request)
+    {
+
+        try {
+            User::create([
+                'firstName' => $request->input('firstName'),
+                'lastName' => $request->input('lastName'),
+                'email' => $request->input('email'),
+                'phone' => $request->input('phone'),
+                'password' => $request->input('password'),
+            ]);
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'User Registration Successfully ',
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'success',
+                'message' => 'User Registration Successfully ',
+                // 'message' => $e->getMessage(),
+            ]);
+        }
+    }
+}
